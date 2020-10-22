699deb8899e7db7fa5f4aa9b940b93427536bc00
minebook <2420505078@qq.com>
2020-10-22 13:59:53 +0800

    first  day1022afternoon

--- src/test/resources/db.sql
+++ src/test/resources/db.sql
@@ -1,8 +1,8 @@
 # 首先创建一个数据库(Tab补全)
-drop database if exists java6ssm;
-create database if not exists java6ssm;
+drop database if exists ele;
+create database if not exists ele;
 
-use java6ssm;
+use ele;
 
 # 创建管理用户数据表
 drop table if exists adminuser;
@@ -95,65 +95,187 @@ DELIMITER ;
 CALL customer_insert ();
 
 
+
+
+
+
 drop table if exists storeinfo;
 create table if not exists storeinfo
 (
-    sid    int          not null comment '商家id'
+    sid    integer auto_increment comment '商家id'
         primary key,
     sname       varchar(50)  not null comment '商家名称',
     spassword   varchar(32)  not null comment '商家密码',
-    sphoto      varchar(128) not null comment '图片',
-    marking     varchar(12)  not null comment '评分',
-    dispatching varchar(32)  not null comment '配送方式',
-    offer       varchar(32)  null comment '推荐字段'
-)
-    comment '商家信息';
+    sphoto      varchar(128) default null comment '图片',
+    marking     varchar(12)  default null comment '评分',
+    dispatching varchar(32)  default null comment '配送方式',
+    offer       varchar(32)  default null comment '推荐字段',
+    add_time timestamp   not null default current_timestamp comment '创建时间',
+    up_time  timestamp   not null default current_timestamp on update current_timestamp comment '修改时间'
+) auto_increment=1 charset=utf8 comment '商家信息表';
+    commit;
+# 通过批处理插入测试数据
+DROP PROCEDURE
+    IF
+        EXISTS storeinfo_insert;
 
+DELIMITER $$
+CREATE PROCEDURE storeinfo_insert ()
+BEGIN
+    DECLARE
+        i INT DEFAULT 1;
+    WHILE
+            i < 100 DO
+            insert into storeinfo (sname,spassword) value (concat('sto',i),'696aa7bb5ee7ac9135f7ed4ef526fe4d') ;
+            SET i = i + 1;
+
+        END WHILE;
+    COMMIT;
+
+END $$
+DELIMITER ;
+CALL storeinfo_insert();
 
 
 drop table if exists active;
 create table if not exists active
 (
-    aid    int          not null comment '活动id'
+    aid    integer auto_increment comment '活动id'
         primary key,
     aname  varchar(50)  not null comment '活动名称',
-    aintro varchar(128) not null comment '活动简介'
-)
-    comment '活动';
+    aintro varchar(128) default null comment '活动简介',
+    add_time timestamp   not null default current_timestamp comment '创建时间',
+    up_time  timestamp   not null default current_timestamp on update current_timestamp comment '修改时间'
+) auto_increment=1 charset=utf8 comment '商家信息表';
+commit;
+# 通过批处理插入测试数据
+DROP PROCEDURE
+    IF
+        EXISTS active_insert;
 
+DELIMITER $$
+CREATE PROCEDURE active_insert ()
+BEGIN
+    DECLARE
+        i INT DEFAULT 1;
+    WHILE
+            i < 100 DO
+            insert into active(aname,aintro) value (concat('act',i),'饿了么首单用户立减9元') ;
+            SET i = i + 1;
+
+        END WHILE;
+    COMMIT;
+
+END $$
+DELIMITER ;
+CALL active_insert();
 
 
 drop table if exists st_ac;
 create table if not exists st_ac
 (
-    stid int not null comment '商家活动表id'
+    stid integer auto_increment comment '商家活动表id'
         primary key,
     fsid int not null comment '商家表的外键',
-    faid int not null comment '活动表的外键'
-)
-    comment '商家活动表';
+    faid int not null comment '活动表的外键',
+#     constraint fk_stac_fsid_to_st_sid
+#         foreign key (fsid) references storeinfo (sid),
+#     constraint fk_stac_faid_to_ac_aid
+#         foreign key (faid) references active (aid),
+    add_time timestamp   not null default current_timestamp comment '创建时间',
+    up_time  timestamp   not null default current_timestamp on update current_timestamp comment '修改时间'
+) auto_increment=1 charset=utf8 comment '商家信息表';
+commit;
+# 通过批处理插入测试数据
+DROP PROCEDURE
+    IF
+        EXISTS st_ac_insert;
 
+DELIMITER $$
+CREATE PROCEDURE st_ac_insert ()
+BEGIN
+    DECLARE
+        i INT DEFAULT 1;
+    WHILE
+            i < 100 DO
+            insert into st_ac(fsid, faid) value (i,i) ;
+            SET i = i + 1;
+
+        END WHILE;
+    COMMIT;
+
+END $$
+DELIMITER ;
+CALL st_ac_insert();
 
 
 drop table if exists goods;
 create table if not exists goods
 (
-    gid    int          not null comment '商品id'
+    gid    integer auto_increment comment '商品id'
         primary key,
     gname  varchar(50)  not null comment '商品名称',
-    ginfo  varchar(128) not null comment '商品介绍',
-    gphoto varchar(128) not null comment '商品图片',
-    gprice double       not null comment '商品价格',
-    gtype  varchar(20)  not null comment '商品所属分类'
-)
-    comment '商品表';
+    ginfo  varchar(128) default null comment '商品介绍',
+    gphoto varchar(128) default null comment '商品图片',
+    gprice double       default null comment '商品价格',
+    gtid  integer  not null comment '商品所属分类',
+    constraint fk_go_gtid_to_ty_tid
+        foreign key (gtid) references typeofgoods (tid),
+    add_time timestamp   not null default current_timestamp comment '创建时间',
+    up_time  timestamp   not null default current_timestamp on update current_timestamp comment '修改时间'
+) auto_increment=1 charset=utf8 comment '商家信息表';
+commit;
+# 通过批处理插入测试数据
+DROP PROCEDURE
+    IF
+        EXISTS goods_insert;
 
+DELIMITER $$
+CREATE PROCEDURE goods_insert ()
+BEGIN
+    DECLARE
+        i INT DEFAULT 1;
+    WHILE
+            i < 100 DO
+            insert into goods (gname,gtid) value (concat('good',i),1) ;
+            SET i = i + 1;
+
+        END WHILE;
+    COMMIT;
+
+END $$
+DELIMITER ;
+CALL goods_insert();
 
 drop table if exists typeofgoods;
 create table if not exists typeofgoods
 (
-    tid   int         not null comment '商品类别id'
+    tid   integer auto_increment comment '商品类别id'
         primary key,
-    tname varchar(20) not null comment '商品分类名称'
-)
-    comment '商品类别表';
+    tname varchar(20) not null comment '商品分类名称',
+    tinfo varchar(50) default null comment '商品分类信息',
+    add_time timestamp   not null default current_timestamp comment '创建时间',
+    up_time  timestamp   not null default current_timestamp on update current_timestamp comment '修改时间'
+) auto_increment=1 charset=utf8 comment '商家信息表';
+commit;
+# 通过批处理插入测试数据
+DROP PROCEDURE
+    IF
+        EXISTS typeofgoods_insert;
+
+DELIMITER $$
+CREATE PROCEDURE typeofgoods_insert ()
+BEGIN
+    DECLARE
+        i INT DEFAULT 1;
+    WHILE
+            i < 100 DO
+            insert into typeofgoods (tname) value (concat('typ',i)) ;
+            SET i = i + 1;
+
+        END WHILE;
+    COMMIT;
+
+END $$
+DELIMITER ;
+CALL typeofgoods_insert();
